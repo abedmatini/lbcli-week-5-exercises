a361e82ec70f8e91f0fb361e341f29fb436f9c97
Abed Matini <abed.matini@gmail.com>
2026-04-16 14:01:13 +0000

    exercise done

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,8 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+# Create a CLTV script with a timestamp of 1495584032 and public key below:
+publicKey="02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"
+locktime="1495584032"
+
+# Combine opcodes and data into the final raw hex script
+echo "0420e52459b17521${publicKey}ac"
