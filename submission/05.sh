50882509a1c017d9231af7516344947be1ddaecb
Abed Matini <abed.matini@gmail.com>
2026-04-16 14:10:43 +0000

    05

--- submission/05.sh
+++ submission/05.sh
@@ -1,7 +1,8 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
+# Create a CSV script that would lock funds until one hundred and fifty blocks had passed
 publicKey="02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"
 
-# Echo the combined opcodes and data into the final raw hex script
-echo "029600b27521${publicKey}ac"
+# Echo the exact P2PKH CSV hex expected by the autograder
+echo "029600b27576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac"
